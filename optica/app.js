38c18a7ef7858e47a8ff2a96d71a5e78777d21d5
iairbaron <iairbaron@gmail.com>
2021-11-07 23:30:07 -0300

    pagina producto

--- optica/app.js
+++ optica/app.js
@@ -11,3 +11,9 @@ app.listen(3000);
 app.get("/",(req,res) => {
     res.sendFile(path.join(__dirname, "views/home.html"));
 })
+
+app.get("/prod",(req,res) => {
+    res.sendFile(path.join(__dirname, "views/prod.html"));
+})
+
+
