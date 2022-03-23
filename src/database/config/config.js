ea6466218db0c344018e84ecc67320b206aeae2f
Tats01 <martin.farres@gmail.com>
2022-03-23 05:45:00 -0300

    heroku

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,21 +1,21 @@
 module.exports = {
     development: {
         username: "b03867688e1607",
-        password: '1f7c8bb9',
+        password: "1f7c8bb9",
         database: "heroku_4c4adcad9a0114c",
         host: "us-cdbr-east-05.cleardb.net",
         dialect: "mysql",
     },
     test: {
         username: "b03867688e1607",
-        password: '1f7c8bb9',
+        password: "1f7c8bb9",
         database: "heroku_4c4adcad9a0114c",
         host: "us-cdbr-east-05.cleardb.net",
         dialect: "mysql",
     },
     production: {
         username: "b03867688e1607",
-        password: '1f7c8bb9',
+        password: "1f7c8bb9",
         database: "heroku_4c4adcad9a0114c",
         host: "us-cdbr-east-05.cleardb.net",
         dialect: "mysql",
