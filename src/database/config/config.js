04da464c2f55ce262f2c030fec7f199c29a720d1
Tats01 <martin.farres@gmail.com>
2022-03-28 20:58:51 -0300

    config

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,23 +1,23 @@
 module.exports = {
     development: {
-        username: "root",
-        password: null,
-        database: "mirrorlens2",
-        host: "127.0.0.1",
+        username: "b03867688e1607",
+        password: "1f7c8bb9",
+        database: "heroku_4c4adcad9a0114c",
+        host: "us-cdbr-east-05.cleardb.net",
         dialect: "mysql",
     },
     test: {
-        username: "root",
-        password: null,
-        database: "database_test",
-        host: "127.0.0.1",
+        username: "b03867688e1607",
+        password: "1f7c8bb9",
+        database: "heroku_4c4adcad9a0114c",
+        host: "us-cdbr-east-05.cleardb.net",
         dialect: "mysql",
     },
     production: {
-        username: "root",
-        password: null,
-        database: "database_production",
-        host: "127.0.0.1",
+        username: "b03867688e1607",
+        password: "1f7c8bb9",
+        database: "heroku_4c4adcad9a0114c",
+        host: "us-cdbr-east-05.cleardb.net",
         dialect: "mysql",
     },
-};
+};  
